9f2e8d06b865f484a14f09373a86f6edee49a359
akhdanrgya <akhdan.anargya@gmail.com>
2024-09-24 21:20:08 +0700

    update layout dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,11 +6,17 @@ import React, { useState, useEffect } from "react";
 import ProductCard from "@/components/productCard";
 
 export default function Dashboard() {
-
   return (
-    <div>
-      <h1 className={title()}>Dashboard</h1>
-      <ProductCard/>
-    </div>
+    <section>
+      <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title()}>Dashboard</h1>
+        </div>
+
+        <ProductCard />
+        
+      </div>
+    </section>
   );
 }
